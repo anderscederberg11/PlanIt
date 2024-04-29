3af3ef7532526de47e25ee7899790cb5f9040468
BwesewB <sebastianfok@gmail.com>
2024-04-29 10:08:27 -0700

    added logo for browser

--- components/HeadArea/index.js
+++ components/HeadArea/index.js
@@ -8,7 +8,7 @@ export default function HeadArea({ title, description }){
                 <meta name="author" content= "Sebastian Fok, Anders Cederberg"/>
                 <meta name="description" content={description}/>
                 <meta name="viewport" content="width=device-width, initial-scale=1"/>
-                <link rel="icon" href="/favicon.ico"/>
+                <link rel="icon" href="/PlanItLogo.png"/>
             </Head>
         </>
     );
