75c62ea935d130b94b5aaf559a9901d1487bbe78
anderscederberg11 <acederberg@my.bcit.ca>
2024-04-29 00:00:44 -0700

    about page complete

--- pages/about.js
+++ pages/about.js
@@ -37,10 +37,46 @@ export default function About(){
                             </defs>
                         </svg>
                     </div>
-                    <div className={styles.sebCircleContainer}>
-                            
-                    </div>
                     <div className={styles.separator}></div>
+                    <div className={styles.sebCircleContainer}>
+                        <img style={{width: '100%', height: '100%'}} alt="Astronaut in orange suit" src='/images/sebastro3.png' />  
+                    </div>
+                    <div className={styles.sebPillContainer}>
+                        <img style={{width: '100%', height: '100%'}} alt="Astronaut in orange suit" src='/images/sebasstro2.png' />  
+                    </div>
+                    <div className={styles.andersContainer}>
+                        <div className={styles.andersMain}>
+                            <img style={{width: '100%', height: '100%'}} alt="Anders B&W Standing" src='/images/andersmain.png' />  
+                        </div>
+                        <div className={styles.andersCircle}>
+                            <img style={{width: '100%', height: '100%'}} alt="Anders listening to music" src='/images/anderscircle.png' />  
+                        </div>
+                        <div className={styles.andersTextContainer}>
+                            <svg xmlns="http://www.w3.org/2000/svg" width="282" height="462" viewBox="0 0 282 462" fill="none">
+                                <g filter="url(#filter0_b_1065_163)">
+                                    <path fill-rule="evenodd" clip-rule="evenodd" d="M41 1C18.9086 1 1 18.9086 1 41V81C1 82.8371 1.12385 84.6453 1.36365 86.4167C1 96.5298 1 108.617 1 123.5V186C1 219.137 27.8629 246 61 246C94.1371 246 121 272.863 121 306V381C121 425.183 156.817 461 201 461C245.183 461 281 425.183 281 381V129C281 112.176 281 98.5103 280.538 87.0976C280.842 85.1095 281 83.0731 281 81V41C281 18.9086 263.091 1 241 1H41Z" fill="#0C0C0C" fill-opacity="0.7"/>
+                                    <path d="M1.36365 86.4167L2.363 86.4526L2.36607 86.3672L2.35461 86.2825L1.36365 86.4167ZM61 246V247V246ZM201 461V462V461ZM280.538 87.0976L279.55 86.9464L279.535 87.0417L279.539 87.138L280.538 87.0976ZM2 41C2 19.4609 19.4609 2 41 2V0C18.3563 0 0 18.3563 0 41H2ZM2 81V41H0V81H2ZM2.35461 86.2825C2.12081 84.5555 2 82.7921 2 81H0C0 82.8821 0.126884 84.7351 0.372688 86.5508L2.35461 86.2825ZM0.364295 86.3807C-0.000142455 96.5158 0 108.624 0 123.5H2C2 108.611 2.00014 96.5438 2.363 86.4526L0.364295 86.3807ZM0 123.5V186H2V123.5H0ZM0 186C0 219.689 27.3106 247 61 247V245C28.4152 245 2 218.585 2 186H0ZM61 247C93.5848 247 120 273.415 120 306H122C122 272.311 94.6894 245 61 245V247ZM120 306V381H122V306H120ZM120 381C120 425.735 156.265 462 201 462V460C157.369 460 122 424.631 122 381H120ZM201 462C245.735 462 282 425.735 282 381H280C280 424.631 244.63 460 201 460V462ZM282 381V129H280V381H282ZM282 129C282 112.183 282 98.4946 281.537 87.0572L279.539 87.138C280 98.5261 280 112.169 280 129H282ZM280 81C280 83.0224 279.846 85.0081 279.55 86.9464L281.527 87.2488C281.838 85.2108 282 83.1239 282 81H280ZM280 41V81H282V41H280ZM241 2C262.539 2 280 19.4609 280 41H282C282 18.3563 263.644 0 241 0V2ZM41 2H241V0H41V2Z" fill="#343434"/>
+                                </g>
+                                <defs>
+                                    <filter id="filter0_b_1065_163" x="-5" y="-5" width="292" height="472" filterUnits="userSpaceOnUse" color-interpolation-filters="sRGB">
+                                    <feFlood flood-opacity="0" result="BackgroundImageFix"/>
+                                    <feGaussianBlur in="BackgroundImageFix" stdDeviation="2.5"/>
+                                    <feComposite in2="SourceAlpha" operator="in" result="effect1_backgroundBlur_1065_163"/>
+                                    <feBlend mode="normal" in="SourceGraphic" in2="effect1_backgroundBlur_1065_163" result="shape"/>
+                                    </filter>
+                                </defs>
+                            </svg>
+                            <h3 className={styles.andersName}>ANDERS CEDERBERG</h3>
+                            <p className={styles.andersFavourites}>
+                                <span className={styles.favourite2}>Favorite...</span><br></br>
+                                Eco Brand: Arcteryx<br></br>
+                                Design Inspo: Minimalism<br></br>
+                                Software: Illustrator<br></br> 
+                                Animal: Shark<br></br>
+                                Icon: Profile<br></br>
+                            </p>
+                        </div>
+                    </div>
                 </div>
             </main>
         </>
