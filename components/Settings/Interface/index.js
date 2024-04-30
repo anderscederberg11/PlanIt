a782893edc3cc0b692a494bf43671ec46373236d
anderscederberg11 <acederberg@my.bcit.ca>
2024-04-29 18:44:05 -0700

    Settings dark mode switch working

--- components/Settings/Interface/index.js
+++ components/Settings/Interface/index.js
@@ -1,14 +1,15 @@
 import styles from "./Interface.module.css"
 import React from "react"
+import { InterfaceMode } from "../ModeSwitch"
 
 export default function Interface() {
 
     return(
         <>
             <div className={styles.interfaceContainer}>
-                <p className={styles.interfaceText}>Interface</p>
-
+                <InterfaceMode property1="default"/>
             </div>
+            
         </>
     )
 }
--- components/Settings/index.js
+++ components/Settings/index.js
@@ -13,7 +13,7 @@ export default function Settings() {
                     <div className={styles.settingsContainer}>
                         <div className={styles.settingsHeader}>
                             <svg xmlns="http://www.w3.org/2000/svg" width="8" height="14" viewBox="0 0 8 14" fill="none" className={styles.backArrow}>
-                                <path d="M7 1L1 7L7 13" stroke="#0BAA81" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                                <path d="M7 1L1 7L7 13" stroke="#0BAA81" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
                             </svg>
                             <p className={styles.settingsText}>Settings</p>
                             <Volume/>
