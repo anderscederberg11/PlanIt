08617b9e750cef484332877c437c51564a320c80
anderscederberg11 <acederberg@my.bcit.ca>
2024-05-08 12:58:37 -0700

    about page scroll fixed

--- components/Footer/index.js
+++ components/Footer/index.js
@@ -2,14 +2,16 @@ import About from '@/pages/about'
 import styles from './Footer.module.css'
 import Link from 'next/link'
 
-export default function Footer({ handleActionPlannerClick, handleExplorePageClick, handleAboutButtonClick }){
+export default function Footer({ handleActionPlannerClick, handleExplorePageClick}){
     return(
         <>
             <footer className={styles.footer}>
                 <div className={styles.footerContainer}>
                     <button onClick={handleActionPlannerClick} className={styles.footerButton}>PLAN</button>
                     <button onClick={handleExplorePageClick} className={styles.footerButton}>EXPLORE</button>
-                    <button onClick={handleAboutButtonClick} className={styles.footerButton}>ABOUT</button>
+                    <Link href="/about">
+                        <button className={styles.footerButton}>ABOUT</button>
+                    </Link>             
                 </div>
             </footer>
         </>
